4da8196aa95869a146cc22a700eda0d32cf05a40
Martin Vysny <martin.vysny@innovatrics.com>
2010-09-28 11:46:17 +0200

    Added getPixel() to RawImage; toBufferedImage() made more effective

--- src/main/java/com/innovatrics/iseglib/RawImage.java
+++ src/main/java/com/innovatrics/iseglib/RawImage.java
@@ -1,5 +1,6 @@
 package com.innovatrics.iseglib;
 
+import java.awt.Dimension;
 import java.awt.Transparency;
 import java.awt.color.ColorSpace;
 import java.awt.image.BufferedImage;
@@ -21,12 +22,12 @@ public class RawImage {
      * Creates a new raw image.
      * @param width The number of pixels indicating the width of the image
      * @param height The number of pixels indicating the height of the image
-     * @param rawImage raw image in 8-bit greyscale raw format (0=black, 255=white),
+     * @param rawImage raw image in 8-bit grayscale raw format (0=black, 127=gray, 255=white),
      */
     public RawImage(int width, int height, byte[] rawImage) {
-        this.width = width;
-        this.height = height;
-        this.rawImage = rawImage;
+	this.width = width;
+	this.height = height;
+	this.image = rawImage;
     }
     /**
      * The number of pixels indicating the width of the image
@@ -39,21 +40,37 @@ public class RawImage {
     /**
      *  raw image in 8-bit raw format,
      */
-    public final byte[] rawImage;
+    public final byte[] image;
 
     @Override
     public String toString() {
-        return "RawImage{" + width + "x" + height + '}';
+	return "RawImage{" + width + "x" + height + '}';
     }
 
-    public BufferedImage toImage(){
-	final ColorSpace cs = ColorSpace.getInstance(ColorSpace.CS_GRAY);
-	final int[] nBits = {8};
-	final ColorModel cm = new ComponentColorModel(cs, nBits, false, true, Transparency.OPAQUE, DataBuffer.TYPE_BYTE);
-	final SampleModel sm = cm.createCompatibleSampleModel(width, height);
-	final DataBufferByte db = new DataBufferByte(rawImage, width * height);
+    public BufferedImage toBufferedImage() {
+	final SampleModel sm = DEFAULT_COLOR_MODEL.createCompatibleSampleModel(width, height);
+	final DataBufferByte db = new DataBufferByte(image, width * height);
 	final WritableRaster raster = Raster.createWritableRaster(sm, db, null);
-	final BufferedImage result = new BufferedImage(cm, raster, false, null);
+	final BufferedImage result = new BufferedImage(DEFAULT_COLOR_MODEL, raster, false, null);
 	return result;
+    }
+    private static final ColorModel DEFAULT_COLOR_MODEL = new ComponentColorModel(ColorSpace.getInstance(ColorSpace.CS_GRAY), new int[]{8}, false, true, Transparency.OPAQUE, DataBuffer.TYPE_BYTE);
+
+    /**
+     * Returns a pixel located on x,y coordinates. 
+     * @param x the x coordinate
+     * @param y the y coordinate
+     * @return the pixel value, 0 = black, 127 = gray, 255 = white.
+     */
+    public byte getPixel(int x, int y) {
+	return image[y * width + x];
+    }
+
+    /**
+     * Returns size of the image.
+     * @return the dimension object.
+     */
+    public Dimension getDimension() {
+	return new Dimension(width, height);
     }
 }
